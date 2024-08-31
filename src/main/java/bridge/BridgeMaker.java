bcca85b36f028e1e50a2e730753626c1c543906e
zzioozzioo <zzioo1006@naver.com>
2024-08-31 15:46:41 +0900

    feat(BridgeMaker): complete the method 'makeBridge' that generate random numbers and add them to list

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.BridgeNumberGenerator;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +21,18 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridgeList = new ArrayList<>();
+        int randomNum;
+
+        for (int i = 0; i < size; i++) {
+            randomNum = bridgeNumberGenerator.generate();
+            if (randomNum == 0) {
+                bridgeList.add("D");
+                continue;
+            }
+            bridgeList.add("U");
+        }
+
+        return bridgeList;
     }
 }
