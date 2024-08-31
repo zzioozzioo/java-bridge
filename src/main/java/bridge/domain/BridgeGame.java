0b87d038beb9a767e02120b886221516c98786a0
zzioozzioo <zzioo1006@naver.com>
2024-08-31 20:03:20 +0900

    feat(BridgeGame): complete the function that the player move on the bridge

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,16 +1,31 @@
-package bridge;
+package bridge.domain;
+
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
 
+    List<String> bridge;
+    BridgeMap bridgeMap;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public BridgeMap move(String moving, int index) {
+        if (moving.equals(bridge.get(index))) {
+            bridgeMap.addMap(moving, "O");
+            return bridgeMap;
+        }
+        bridgeMap.addMap(moving, "X");
+        return bridgeMap;
     }
 
     /**
