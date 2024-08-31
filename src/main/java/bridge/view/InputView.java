73897e8776098f6133387bfa138172d60664fd99
zzioozzioo <zzioo1006@naver.com>
2024-08-31 15:43:38 +0900

    feat(InputView): add methods that receive input data from the player

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,35 @@
+package bridge.view;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    // TODO: validation은 다른 패키지에서
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public String readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+        return readLine();
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        return readLine();
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q");
+        return readLine();
+    }
+}
