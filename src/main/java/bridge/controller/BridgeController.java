c4e83f62d6ba1684f33a3c7dd580676e56719bcc
zzioozzioo <zzioo1006@naver.com>
2024-08-31 21:22:41 +0900

    refactor(BridgeController): modify the function that the player cross the bridge and restart the game

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -17,31 +17,31 @@ public class BridgeController {
 
     public void run() {
 
+        startGame();
+        List<String> bridge = makeNewBridge();
+
+        passBridge(bridge);
+
     }
 
     /**
-     * 다리 건너는 동작
+     * 게임 시작
      */
-    public void passBridge() {
-        int bridgeSize = inputView.readBridgeSize();
-        String moving;
-        List<String> bridge = getNewBridge(bridgeSize);
-
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        BridgeMap bridgeMap;
-
-        for (int i = 0; i < bridgeSize; i++) {
-
-            moving = inputView.readMoving();
-            validateMoving(moving);
-
-            bridgeMap = bridgeGame.move(moving, i);
-            outputView.printMap(bridgeMap);
-        }
+    public void startGame() {
+        outputView.printStartGame();
     }
 
-    private static void validateMoving(String moving) {
-        if (!moving.equals("U") && !moving.equals("D")) {
+    /**
+     * 새로운 다리 생성
+     */
+    public List<String> makeNewBridge() {
+        int bridgeSize = inputView.readBridgeSize();
+        isValidSize(bridgeSize);
+        return getNewBridge(bridgeSize);
+    }
+
+    public void isValidSize(int bridgeSize) {
+        if (bridgeSize < 3 || bridgeSize > 20) {
             throw new IllegalArgumentException();
         }
     }
@@ -50,5 +50,82 @@ public class BridgeController {
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
         return bridgeMaker.makeBridge(count);
+    }
+
+    /**
+     * 다리 건너는 동작
+     *
+     */
+    public void passBridge(List<String> bridge) {
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        BridgeMap bridgeMap;
+
+        for (int i = 0; i < bridge.size(); i++) {
+            bridgeMap = passOneKan(bridgeGame, i);
+            if (checkFail(bridge, bridgeMap, bridgeGame))
+                return;
+        }
+        printEndGame(bridgeGame);
+    }
+
+    private boolean checkFail(List<String> bridge, BridgeMap bridgeMap, BridgeGame bridgeGame) {
+        if (bridgeMap.getAllMap().containsValue("X")) {
+            if (restartGame(bridge, bridgeGame)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public BridgeMap passOneKan(BridgeGame bridgeGame, int i) {
+        String moving;
+        BridgeMap bridgeMap;
+
+        moving = inputView.readMoving();
+        isValidMoving(moving);
+
+        bridgeMap = bridgeGame.move(moving, i);
+        outputView.printMap(bridgeMap);
+
+        return bridgeMap;
+    }
+
+    public void isValidMoving(String moving) {
+        if (!moving.equals("U") && !moving.equals("D")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    /**
+     * 게임 재시작
+     *
+     */
+    public boolean restartGame(List<String> bridge, BridgeGame bridgeGame) {
+        String gameCommand = inputView.readGameCommand();
+        isValidGameCommand(gameCommand);
+        if (gameCommand.equals("R")) {
+            bridgeGame.addCount();
+            passBridge(bridge);
+        }
+        if (gameCommand.equals("Q")) {
+            // 게임 최종 결과 출력 및 종료
+            printEndGame(bridgeGame);
+            return false;
+        }
+
+        throw new IllegalArgumentException(); // R, Q 모두 아닌 경우
+    }
+
+    public void isValidGameCommand(String gameCommand) {
+        if (!gameCommand.equals("R") && !gameCommand.equals("Q")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void printEndGame(BridgeGame bridgeGame) {
+        outputView.printResult(); // TODO: 이거 완성하기
+        outputView.printIsSuccess("실패");
+        outputView.printTryCount(bridgeGame);
     }
 }
