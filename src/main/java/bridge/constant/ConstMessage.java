53524f68d64e49e47c9eecbb8b78be97ffeab952
zzioozzioo <zzioo1006@naver.com>
2024-09-01 19:52:36 +0900

    refactor(ConstMessage): add a type to enum

--- src/main/java/bridge/constant/ConstMessage.java
+++ src/main/java/bridge/constant/ConstMessage.java
@@ -2,30 +2,32 @@ package bridge.constant;
 
 public enum ConstMessage {
 
-    UP("U"),
-    DOWN("D"),
+    UP("U", "DIRECTION"),
+    DOWN("D", "DIRECTION"),
 
-    RESTART("R"),
-    QUIT("Q"),
+    RESTART("R", "COMMAND"),
+    QUIT("Q", "COMMAND"),
 
-    SUCCESS("성공"),
-    FAIL("실패"),
+    SUCCESS("성공", "RESULT"),
+    FAIL("실패", "RESULT"),
 
-    POSSIBLE("O"),
-    IMPOSSIBLE("X"),
+    POSSIBLE("O", "STATUS"),
+    IMPOSSIBLE("X", "STATUS"),
 
-    START_GAME("다리 건너기 게임을 시작합니다."),
-    READ_BRIDGE_SIZE("다리의 길이를 입력해주세요."),
-    READ_MOVING("이동할 칸을 선택해주세요. (위: " + ConstMessage.UP.getValue() + ", 아래: " + ConstMessage.DOWN.getValue() + ")"),
-    READ_GAME_COMMAND("게임을 다시 시도할지 여부를 입력해주세요. (재시도: " + ConstMessage.RESTART.getValue() + ", 종료: " + ConstMessage.QUIT.getValue() + ")"),
-    IS_SUCCESS("게임 성공 여부: "),
-    TRY_COUNT("총 시도한 횟수: ");
+    START_GAME("다리 건너기 게임을 시작합니다.", "MESSAGE"),
+    READ_BRIDGE_SIZE("다리의 길이를 입력해주세요.", "MESSAGE"),
+    READ_MOVING("이동할 칸을 선택해주세요. (위: " + ConstMessage.UP.getValue() + ", 아래: " + ConstMessage.DOWN.getValue() + ")", "MESSAGE"),
+    READ_GAME_COMMAND("게임을 다시 시도할지 여부를 입력해주세요. (재시도: " + ConstMessage.RESTART.getValue() + ", 종료: " + ConstMessage.QUIT.getValue() + ")", "MESSAGE"),
+    IS_SUCCESS("게임 성공 여부: ", "MESSAGE"),
+    TRY_COUNT("총 시도한 횟수: ", "MESSAGE");
 
 
     final String value;
+    final String type;
 
-    ConstMessage(String value) {
+    ConstMessage(String value, String type) {
         this.value = value;
+        this.type = type;
     }
 
     public String getValue() {
