dc42de19674dc3b277705cdbdc29013fce68a4cf
zzioozzioo <zzioo1006@naver.com>
2024-09-05 19:21:28 +0900

    test(BridgeMapTest): create additional tests as method in 'BridgeMap' class are segmented

--- src/test/java/bridge/domain/BridgeMapTest.java
+++ src/test/java/bridge/domain/BridgeMapTest.java
@@ -18,26 +18,47 @@ public class BridgeMapTest {
         //given
         BridgeMap bridgeMap = new BridgeMap();
 
-        String firstDirection = "U";
-        String firstStatus = "O";
+        String direction = "u";
+        String status = "O";
 
-        String secondDirection = "F";
-        String secondStatus = "O";
-
-        // 첫 번째 칸 이동
-        //when
-        bridgeMap.addMap(firstDirection, firstStatus);
-
-        //then
-        assertEquals(List.of("O"), bridgeMap.getUpStatus());
-
-        // 두 번째 칸 이동
         //when & then
-        assertThatThrownBy(() -> bridgeMap.addMap(secondDirection, secondStatus))
+        assertThatThrownBy(() -> bridgeMap.addMap(direction, status))
                 .isInstanceOf(InvalidMovingException.class)
                 .hasMessageContaining("[ERROR] 이동할 칸은 "
                         + Direction.UP.getDirection() + " 또는 "
                         + Direction.DOWN.getDirection() + "여야 합니다.");
+    }
+
+    @DisplayName("이동할 칸이 U일 때 맵에 추가 테스트")
+    @Test
+    void UpDirectionTest() {
+        //given
+        BridgeMap bridgeMap = new BridgeMap();
+
+        String direction = "U";
+        String status = "O";
+
+        //when
+        bridgeMap.upDirection(direction, status);
+
+        //then
+        assertEquals(List.of("O"), bridgeMap.getUpStatus());
+    }
+
+    @DisplayName("이동할 칸이 D일 때 맵에 추가 테스트")
+    @Test
+    void DownDirectionTest() {
+        //given
+        BridgeMap bridgeMap = new BridgeMap();
+
+        String direction = "D";
+        String status = "X";
+
+        //when
+        bridgeMap.downDirection(direction, status);
+
+        //then
+        assertEquals(List.of("X"), bridgeMap.getDownStatus());
     }
 
     @DisplayName("맵에 X가 있는지 테스트")
