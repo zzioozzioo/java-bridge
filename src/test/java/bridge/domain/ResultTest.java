b7fe685b5b25f05e9782f5d734ca6d8ce2339916
zzioozzioo <zzioo1006@naver.com>
2024-09-05 18:57:53 +0900

    test(ResultTest): add an unit test for Result class

---
+++ src/test/java/bridge/domain/ResultTest.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static bridge.domain.Result.matchResult;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ResultTest {
+
+    @DisplayName("게임 성공 여부와 Result 상수의 값 매치 테스트")
+    @Test
+    void MatchResultTest() {
+        //given
+        boolean flag = true;
+
+        //when
+        String result = matchResult(flag);
+
+        //then
+        assertEquals("성공", result);
+    }
+}
