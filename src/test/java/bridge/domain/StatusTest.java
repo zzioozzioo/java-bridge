f3b83b095991921810d1bf26b70089895cdd5ca8
zzioozzioo <zzioo1006@naver.com>
2024-09-05 18:58:06 +0900

    test(StatusTest): add an unit test for Result class

---
+++ src/test/java/bridge/domain/StatusTest.java
@@ -0,0 +1,26 @@
+package bridge.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatusTest {
+
+    @DisplayName("현재 이동할 칸과 다리를 비교해 건널 수 있는지 여부 매치 테스트")
+    @Test
+    void MatchStatusTest() {
+        //given
+        String direction = "U";
+        List<String> bridge = List.of("U", "U", "D");
+        int index = 0;
+
+        //when
+        Status status = Status.matchStatus(direction, bridge.get(index));
+
+        //then
+        assertEquals(Status.POSSIBLE, status);
+    }
+}
