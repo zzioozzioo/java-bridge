22f81a680be06e12b3c1a42b79890c2f8146e988
zzioozzioo <zzioo1006@naver.com>
2024-09-05 18:56:56 +0900

    test(DirectionTest): add an unit test for Direction class

---
+++ src/test/java/bridge/domain/DirectionTest.java
@@ -0,0 +1,26 @@
+package bridge.domain;
+
+import bridge.exception.InvalidMovingException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static bridge.domain.Direction.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DirectionTest {
+
+    @DisplayName("플레이어가 입력한 이동할 칸과 Direction 상수 매치 테스트")
+    @Test
+    void MatchDirectionTest() {
+        //given
+        String str = "u";
+
+        //when & then
+        assertThatThrownBy(() -> matchDirection(str))
+                .isInstanceOf(InvalidMovingException.class)
+                .hasMessageContaining("[ERROR] 이동할 칸은 "
+                        + Direction.UP.getDirection() + " 또는 "
+                        + Direction.DOWN.getDirection() + "여야 합니다.");
+    }
+}
