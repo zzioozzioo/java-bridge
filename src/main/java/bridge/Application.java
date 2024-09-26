0e7d6dc66242fc314f7f5a8d29a1967669df8f92
zzioozzioo <zzioo1006@naver.com>
2024-09-26 18:48:59 +0900

    fix: inject dependencies of objects in 'Application'

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,12 +1,21 @@
 package bridge;
 
 import bridge.controller.BridgeController;
+import bridge.domain.BridgeGame;
+import bridge.service.BridgeService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        BridgeController bridgeController = new BridgeController();
+        List<String> bridge = new ArrayList<>();
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        BridgeService bridgeService = new BridgeService(bridgeGame);
+        BridgeController bridgeController = new BridgeController(bridgeService);
+
         bridgeController.run();
     }
 }
